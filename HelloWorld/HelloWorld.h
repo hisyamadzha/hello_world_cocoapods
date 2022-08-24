05307d6660840030c3d871e018f727eeb41f1db9
hisyam <hisyam@molpay.com>
2022-08-24 11:08:07 +0800

    Initial Commit

---
+++ HelloWorld/HelloWorld.h
@@ -0,0 +1,18 @@
+//
+//  HelloWorld.h
+//  HelloWorld
+//
+//  Created by TekJau Tan on 24/08/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HelloWorld.
+FOUNDATION_EXPORT double HelloWorldVersionNumber;
+
+//! Project version string for HelloWorld.
+FOUNDATION_EXPORT const unsigned char HelloWorldVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HelloWorld/PublicHeader.h>
+
+
